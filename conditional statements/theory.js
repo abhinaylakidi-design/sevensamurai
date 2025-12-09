2d245944c59831a50fd4594522f6af2302eddc17
Abhinay <abhinaylakidi@gmail.com>
2025-12-09 22:13:42 +0530

    Conditional statements

---
+++ conditional statements/theory.js
@@ -0,0 +1,29 @@
+// Conditional starements in JavaScript
+
+// 1. if statement
+let age = 18;
+if (age >= 18) {
+  console.log("You are eligible to vote.");
+}
+
+// 2. if-else statement
+let marks = 35;
+if (marks >= 35){
+    console.log("student is pass")
+}
+else
+{
+    "student is fail"
+}
+
+let studentpercentage = 30;
+if (studentpercentage >= 40) {
+    console.log("student is pass")
+}
+else
+{
+    "student is fail"
+}
+
+// 3. if-else if-else statement
+let score = 85;
