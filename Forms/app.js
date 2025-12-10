4faa42306e79a73851fdbdb0749d12cceeacea63
Abhinay <abhinaylakidi@gmail.com>
2025-12-10 11:33:42 +0530

    forms

---
+++ Forms/app.js
@@ -0,0 +1,6 @@
+const form = document.querySelector('form');
+let username=document.getElementById('username').value;
+let email=document.getElementById('email').value;
+let password=document.getElementById('password').value;
+let error=document.getElementById('error');
+
