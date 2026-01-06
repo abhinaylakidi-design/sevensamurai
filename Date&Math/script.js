59b4b6c0d45b11519363edc768c5c7fb3777ad9a
Abhinay <abhinaylakidi@gmail.com>
2026-01-06 13:49:22 +0530

    Date & math

---
+++ Date&Math/script.js
@@ -0,0 +1,44 @@
+
+/*
+Math()
+
+Math.PI
+math.ciel()
+math.floor()
+math.round()
+math.trunc() removes the decimal points
+math.pow(x,y)
+math.sqrt(x)
+math.random() generates the random number b/w 0 & 1
+
+
+
+*/
+
+console.log(Math.ceil(4.515));
+console.log(Math.floor(4.515));
+console.log(Math.round(4.515));
+console.log(Math.trunc(4.515));
+console.log(Math.pow(2,3));
+console.log(Math.sqrt(25));
+console.log(Math.random())
+
+
+/*
+Date is a constructer
+
+getdate()
+getfullyear()
+gethours()
+getday()
+setdate()
+setmonth()
+setfullyear()
+date.toDateString()
+date.toLocaleDateString()
+
+
+*/
+
+let date = new Date();
+console.log(date)
